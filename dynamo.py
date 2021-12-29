843866b1bd7708a1b73eaa4d7528d4685ef4d616
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-12-30 02:34:16 +0900

    firstcommit

---
+++ dynamo.py
@@ -0,0 +1,27 @@
+import boto3
+
+class Dynamo:
+    def __init__(self, table):
+        self.setDynamo(table)
+
+    def getItems(self):
+        items = self.table.scan()
+        return items['Items']
+
+
+    def getItem(self, id):
+        item = self.table.get_item(Key={'id': id})
+        return item['Item']
+
+    def setDynamo(self,table):
+        dynamodb = boto3.resource(
+            'dynamodb',
+            endpoint_url="http://localhost:8000",
+            region_name='local',
+            aws_access_key_id='ACCESS_ID',
+            aws_secret_access_key='ACCESS_KEY')
+
+        self.table = dynamodb.Table(table)
+
+dynamo = Dynamo('miko')
+print(dynamo.getItems())
